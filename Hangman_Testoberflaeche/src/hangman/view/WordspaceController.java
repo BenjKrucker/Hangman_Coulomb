09bae57d075161caed80499931b120cedcacd8e1
J-Anderrueti <julien.anderrueti@hotmail.com>
2018-12-14 12:35:30 +0100

    with input textfield working

--- Hangman_Testoberflaeche/src/hangman/view/WordspaceController.java
+++ Hangman_Testoberflaeche/src/hangman/view/WordspaceController.java
@@ -2,7 +2,6 @@ package hangman.view;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import hangman.MainApp;
 import hangman.view.KeyboardController;
 import hangman.model.Key;
@@ -10,21 +9,37 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class WordspaceController {
 
 	static String word = "Beispiel";
 	static ArrayList<String> input = new ArrayList<String>();
 	static ArrayList<String> guess = new ArrayList<String>();
-	//Key test = new Key();
 
 	@FXML
 	public Label guessWord;
 	
-	//@ FXML
-	//public Textfield wordToGuess;
+	@FXML
+	public TextField wordToGuess;
+	
+	@FXML
+	public void onEnter() {
+		word = wordToGuess.getText();
+		convertInput();
+		guessWord.setText(createEmptyWord());
+		wordToGuess.clear();
+		wordToGuess.setText("Gesuchtes Wort");
+		wordToGuess.setDisable(true);
+		
+	}
 
 	private MainApp mainApp;
 
@@ -40,22 +55,18 @@ public class WordspaceController {
 	// initialize
 	@FXML
 	private void initialize() {
-		convertInput();
-		createEmptyWord();
-		setWordUpdate("Give a word to guess!");
+		
+		guessWord.setText("Give a word to guess!");
 	}
 
 	// set word
 
 	
-	public void setWordUpdate(String wordUpdate){
+	/*public void setWordUpdate(String wordUpdate){
 		guessWord.setText(wordUpdate);
-	}
+	}*/
 
 	private void convertInput() {
-
-		// include value from word library here
-		 //word = " Beispiel ";
 
 		// convert word into char with spaces between letters
 		for (int n = 0; n < 2 * word.length(); n = n + 2) {
@@ -63,17 +74,11 @@ public class WordspaceController {
 			input.add(n + 1, " ");
 		}
 		input.replaceAll(String::toUpperCase);
-		//String out = String.join("", input);
-		//System.out.println(out);
 
-		// String output = "Gulasch";
-
-		// return output;
 	}
 
 	public  String createEmptyWord() {
-
-		// String qiu = "gulasch";
+		
 		// create empty char which looks like this: _ _ _ _ _ _
 		for (int i = 0; i < 2 * word.length(); i = i + 2) {
 			guess.add(i, "_");
@@ -82,35 +87,36 @@ public class WordspaceController {
 		// String output = new String(guess);
 
 		String output = String.join("", guess);
-		System.out.println(output);
 		return output;
 	}
 	
 
 	public void testNewLetter(char letter) {
 		
-		//  Counter !!
+		int counter = 0;
 		
-		//char letter = test.getIdentity();
-		
-		// char letterChoice = key;
+
 		String letterChoice = Character.toString(letter);
-		// String letterChoice = String.valueOf(letter);
+
 
 		// compare letters with selected letter
 		for (int j = 0; j < 2 * word.length(); j = j + 2) {
 			if (input.get(j).equals(letterChoice)) {
 				guess.set(j, letterChoice);
 			}
+			else {counter++;}
 		}
+		
+		//bring counter to picture
 
 		// return adapted char as string value
-		 //String outcome = Arrays.toString(guess.toArray());
 		String outcome = String.join("", guess);
-		System.out.println(outcome);
-		//setWordUpdate(outcome);
 		guessWord.setText(outcome);
 		
 	}
+
+	
+	
+	
 
 }
